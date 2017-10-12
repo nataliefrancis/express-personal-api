193f0618352bc6fcead71807b42d2b14bf56305d
nataliefrancis <natdfrancis@gmail.com>
2017-10-11 19:07:23 -0600

    starts db info

--- seed.js
+++ seed.js
@@ -1,11 +1,45 @@
 // This file allows us to seed our application with data
 // simply run: `node seed.js` from the root of this project folder.
 
-// var db = require('./models');
+var db = require('./models');
 
-// var new_campsite = {description: "Sharp rocks. Middle of nowhere."}
+var vacation_list = [
+	{
+	city: "Paris, France",
+	description: "Visited the Louvre, Musee D'Orsay, Eiffel Tower, and Montmontre",
+	visited: true
+	},
+	{
+	city: "London, England",
+	description: "Visited the Tower of London, Stonehenge (outside of London), and Abbey Road",
+	visited: true
+	},
+	{
+	city: "Amsterdam, Netherlands",
+	description: "Visited Anne Frank's house, ate space cakes",
+	visited: true
+	},
+	{
+	city: "Sydney, Australia",
+	description: "Want to snorkel the Great Barrier Reef",
+	visited: false
+	}
+];
 
-// db.Campsite.create(new_campsite, function(err, campsite){
+db.Vacation.remove({}, function () {
+	console.log("removed all vacations");
+	db.Vacation.create(vacation_list, function (err, vacations) {
+		if (err) {
+			console.log(err);
+			return;
+		}
+		console.log("created ", +vacation.legth+ " vacations");
+	});
+});
+
+// var new_vacation = {description: "Sharp rocks. Middle of nowhere."};
+
+// db.Vacation.create(new_campsite, function(err, campsite){
 //   if (err){
 //     return console.log("Error:", err);
 //   }
